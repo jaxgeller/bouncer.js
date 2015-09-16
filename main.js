9c46f94f992de9f47913e297fbe6eaadb8c940a5
jaxgeller <jacksongeller@gmail.com>
2015-09-15 21:58:44 -0400

    add fire once option, change up api a bit

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ export default class Bouncer {
     this.windowOffset, this.windowBuffer = window.pageYOffset;
     this.offset = (opts.offset / 100) * window.innerHeight || 0;
 
+    this.fireOnce = opts.fireOnce || false;
     window.addEventListener('scroll', this.onScroll.bind(this), false);
   }
 
@@ -14,16 +15,16 @@ export default class Bouncer {
     this.windowOffset = window.pageYOffset + this.offset;
 
     for (let i = 0; i < this.sections.length; i++) {
-      if (this._checkPosition(i)) {
-        this.windowOffset > this.windowBuffer ?
-          this.callback('down') :
-          this.callback('up');
-      }
-    }
+      if (this.fireOnce)
+        this.checkFireOnce(i);
+      else
+        this.checkFire(i);
 
-    this.windowBuffer = this.windowOffset;
-    this.ticking = false;
+      this.windowBuffer = this.windowOffset;
+      this.ticking = false;
+    }
   }
+
 
   onScroll() {
     if(!this.ticking) {
@@ -32,9 +33,31 @@ export default class Bouncer {
     }
   }
 
+  checkFireOnce(i) {
+    if (this._checkPosition(i)) {
+      if (this.canFire) {
+        this.canFire = false;
+        this._reportDirection();
+      }
+    } else {
+      this.canFire = true;
+    }
+  }
+  checkFire(i) {
+    if (this._checkPosition(i))
+      this._reportDirection()
+  }
+
+
   _checkPosition(i) {
     return this.windowOffset > this.sections[i].start
         && this.windowOffset < this.sections[i].end;
+  }
+
+  _reportDirection() {
+    this.windowOffset > this.windowBuffer ?
+      this.callback('down') :
+      this.callback('up');
   }
 
   _setSections(selector) {
